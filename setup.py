0ebfbfb4fc302a27e61beb571b92f507ecbac308
pkegg <pkegg@users.noreply.github.com>
2022-02-03 01:51:40 -0700

    Make a simple setup.py to allow installing dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+setup(name='351elec-webui',
+      version='1.0',
+      description='Web Interface for 351ELEC',
+      author='benphelps',
+      author_email='bphelpsen@gmail.com',
+      url='https://github.com/benphelps/351elec-webui',
+      license='MIT',
+      platforms=['Linux'],
+      install_requires=['ftfy']
+
+      )
