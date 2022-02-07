93dffcfb5f3b2862de9de9f6d04bfce8d1f28673
Ben Phelps <ben@phelps.io>
2022-02-07 12:31:07 +0200

    put back non-debug values

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 host = "0.0.0.0"
 port = 8080
 
-# debug = False
-# reloader = False
+debug = False
+reloader = False
 
-# roms_folder = "/storage/roms/"
-# es_systems_path = "/usr/config/emulationstation/es_systems.cfg"
+roms_folder = "/storage/roms/"
+es_systems_path = "/usr/config/emulationstation/es_systems.cfg"
 
-debug = True
-reloader = True
+# debug = True
+# reloader = True
 
-roms_folder = "/Volumes/GAMES/"
-es_systems_path = "es_systems.cfg"
+# roms_folder = "/Volumes/GAMES/"
+# es_systems_path = "es_systems.cfg"
