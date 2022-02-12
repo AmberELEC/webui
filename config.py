f5caf80aabd5cf33c401b5fc173dfb6922d51bb2
Ben Phelps <ben@phelps.io>
2022-02-12 20:56:20 +0200

    use port 80 on device

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 host = "0.0.0.0"
-port = 8080
-
+port = 80
 debug = False
 reloader = False
-
 roms_folder = "/storage/roms/"
 es_systems_path = "/usr/config/emulationstation/es_systems.cfg"
 
+# host = "127.0.0.1"
+# port = 8080
 # debug = True
 # reloader = True
-
 # roms_folder = "/Volumes/GAMES/"
 # es_systems_path = "es_systems.cfg"
